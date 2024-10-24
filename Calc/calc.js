6810cc72860b7f6ec0b503bfe4c155191068b0a8
Asmiya76 <159458506+Asmiya76@users.noreply.github.com>
2024-10-24 20:00:29 +0530

    Add files via upload

---
+++ Calc/calc.js
@@ -0,0 +1,12 @@
+let calculation = localStorage.getItem('calculation') || '' ;
+      function updateCalculation(value){
+        calculation += value;
+        console.log(calculation);
+        localStorage.setItem('calculation',calculation);
+        displayCalculation();
+      }
+
+
+      function displayCalculation(){
+        document.querySelector('.js-calculation').innerHTML = calculation;
+      }
